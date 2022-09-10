17d0a7f9ed750b614b79272319ad63bdee89c648
Carnot <narayane.rm@gmail.com>
2022-09-10 19:34:34 -0300

    Initial Rescue

--- src/components/Rescue.tsx
+++ src/components/Rescue.tsx
@@ -4,14 +4,32 @@ import Header from "./Header";
 
 export default function Home(){
     return(
-        <HomeStyle>
-            <Header/>
-        </HomeStyle>
+        <RescueStyle>
+        </RescueStyle>
     );
 }
 
-const HomeStyle = styled.div`
-    display: flex;
+const RescueStyle = styled.div`
+    display: fixed;
     flex-direction: column;
+    height: 300px;
+    width: 10%;
+
+`;
+const HeaderStyle = styled.div`
+    display: flex;
+    flex-direction: row;
+    top: 0;
+    left: 0;
+    right: 0;
+    width: 100%;
+    height: 100px;
+    border-bottom: 1px solid gray;
+    padding-top: 25px;
+    padding-left: 1.8%;
+    padding-right: 1.8%;
+    padding-bottom: 10px;
+    align-items: center;
+    justify-content: space-between;
 
 `;
