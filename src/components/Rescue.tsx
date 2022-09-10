380b9a6316cf9e059da9c96220b241f0665e9160
Carnot <narayane.rm@gmail.com>
2022-09-10 19:21:21 -0300

    Rescue initial page

---
+++ src/components/Rescue.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import styled from "styled-components";
+import Header from "./Header";
+
+export default function Home(){
+    return(
+        <HomeStyle>
+            <Header/>
+        </HomeStyle>
+    );
+}
+
+const HomeStyle = styled.div`
+    display: flex;
+    flex-direction: column;
+
+`;
