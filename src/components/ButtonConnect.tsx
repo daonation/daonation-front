6cbc6f27a26b4f0a3640297347bed95e2736ffb0
Luiz <lvj253@gmail.com>
2022-09-10 19:15:14 -0300

    links header

--- src/components/ButtonConnect.tsx
+++ src/components/ButtonConnect.tsx
@@ -1,10 +1,35 @@
 import React from "react";
+import internal from "stream";
 import styled from "styled-components";
 
-export default function ButtonConnect(){
+interface propsButton {
+    height: number,
+    width: number
+}
+
+export default function ButtonConnect(  props:propsButton ){
     return (
-        <ConnectStyle>Conectar Carteira</ConnectStyle>
+        <ConnectStyle height={props.height} width={props.width}>
+            <h1>CONECTAR WALLET</h1>
+        </ConnectStyle>
     );
 }
 
-const ConnectStyle = styled.div``;
+const ConnectStyle = styled.div<{height: number; width: number;}>`
+    background: #2BFFD9;
+    height: ${props => props.height}px;
+    width: ${props => props.width}px;
+    color: #000000;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    border-radius: ${props => props.width/15}px;
+    cursor: pointer;
+    h1{
+        display: block;
+        color: #000000;
+        :hover{
+            text-decoration: none;
+        }
+    }
+`;
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,18 +1,21 @@
 import React from "react";
 import styled from "styled-components";
 import ButtonConnect from "./ButtonConnect";
+import {useNavigate} from "react-router-dom";
 
 export default function Header(){
+
+    const history = useNavigate();
     return(
         
         <HeaderStyle>
             <LogoImg src={"/static/logoHome.png"} alt=""/>
-            <ButtonList>
-                <h1>Doe</h1>
-                <h2>Em votação</h2>
-                <h2>Resgate tokens</h2>
-                <h2>DAO</h2>
-                <ButtonConnect/>
+            <ButtonList >
+                <h1 onClick={() => history("/")} >Doe</h1>
+                <h2 onClick={() => history("/vote")}>Em votação</h2>
+                <h2 onClick={() => history("/redeem")}>Resgate tokens</h2>
+                <h2 onClick={() => history("/dao")}>DAO</h2>
+                <ButtonConnect height={75} width={240}/>
 
             </ButtonList>
         </HeaderStyle>
@@ -31,6 +34,7 @@ const HeaderStyle = styled.div`
     padding-top: 25px;
     padding-left: 1.8%;
     padding-right: 1.8%;
+    padding-bottom: 10px;
     align-items: center;
     justify-content: space-between;
 
@@ -49,6 +53,10 @@ const ButtonList = styled.div`
     width: 60%;
     h1,h2{
         font-size: 21px;
+        :hover{
+            text-decoration: underline;
+            cursor: pointer;
+        }
     }
     h1{
         color: #2BFFD9;
